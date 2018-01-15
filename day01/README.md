8f8c9dac61149b9090f8427e0d90da26692eefc6
Eduardo Ramirez <edramir18@gmail.com>
2018-01-15 18:51:59 -0400

    Fix playDrumKit function, added click mouse event

--- day01/README.md
+++ day01/README.md
@@ -1,6 +1,7 @@
 # DAY 1 - JavaScript Drum Kit
 
 ## Documentation
+- [getAttribute](https://www.w3schools.com/jsref/met_element_getattribute.asp)
 - [Audio Tag](https://www.w3schools.com/tags/tag_audio.asp)
 - [Audio DOM Reference](https://www.w3schools.com/tags/ref_av_dom.asp)
 
--- day01/js/main.js
+++ day01/js/main.js
@@ -1,12 +1,16 @@
-function keyDrumKitDown(e) {
-    const key = e.keyCode;
-    const drum = document.querySelector(`.key[data-key="${key}"]`);
-    const sound = document.querySelector(`audio[data-key="${key}"]`);
-    if(!drum || !sound) return;    
+function playDrumkit(keyCode){
+    const drum = document.querySelector(`.key[data-key="${keyCode}"]`);
+    const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
+    if(!drum || !sound) return;
     sound.currentTime = 0;
     sound.play();
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-		window.addEventListener("keydown", keyDrumKitDown);
-	}, false);
+        window.addEventListener("keydown", (e) => playDrumkit(e.keyCode));
+        const keys = document.querySelectorAll('.key');
+        keys.forEach((k) => {
+            k.addEventListener("click", (e) => playDrumkit(e.currentTarget.getAttribute("data-key")));
+        });
+
+    }, false);
