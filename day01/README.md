bc973a9168b1160590fd9597e90cdefa9f1906c9
Eduardo Ramirez <edramir18@gmail.com>
2018-01-15 22:01:56 -0400

    Day 1 Version 1.0

--- day01/README.md
+++ day01/README.md
@@ -1,7 +1,17 @@
 # DAY 1 - JavaScript Drum Kit
+This project was make with the help of CSS Grid, to generate the layout of the components.
+Still look ugly but its working according the challenge.
+
+I added the click event to work on mobile devices as well.
+
+_BUGS_
+In case a key is pressed repeatedly the class *playing* is no removed;
 
 ## Documentation
-- [getAttribute](https://www.w3schools.com/jsref/met_element_getattribute.asp)
+- [Transitioned Event](https://developer.mozilla.org/es/docs/Web/Reference/Events/transitionend)
+- [Add a CSS class](https://www.w3schools.com/howto/howto_js_add_class.asp)
+- [Remove a CSS class](https://www.w3schools.com/howto/howto_js_remove_class.asp)
+- [Get a HTML attribute](https://www.w3schools.com/jsref/met_element_getattribute.asp)
 - [Audio Tag](https://www.w3schools.com/tags/tag_audio.asp)
 - [Audio DOM Reference](https://www.w3schools.com/tags/ref_av_dom.asp)
 
--- day01/js/main.js
+++ day01/js/main.js
@@ -1,16 +1,27 @@
-function playDrumkit(keyCode){
+function playDrumKitAudio(keyCode){
     const drum = document.querySelector(`.key[data-key="${keyCode}"]`);
     const sound = document.querySelector(`audio[data-key="${keyCode}"]`);
     if(!drum || !sound) return;
+    // Added Class to active the key
+    drum.classList.add('playing');    
+    // Rewind the audio in case is playing
     sound.currentTime = 0;
     sound.play();
 }
 
+function resetDrumkitKeyClass(drumkit, property){    
+    if(property !== 'transform'){
+        return;
+    }
+    drumkit.classList.remove("playing");
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
-        window.addEventListener("keydown", (e) => playDrumkit(e.keyCode));
+        window.addEventListener("keydown", (e) => playDrumKitAudio(e.keyCode));
         const keys = document.querySelectorAll('.key');
         keys.forEach((k) => {
-            k.addEventListener("click", (e) => playDrumkit(e.currentTarget.getAttribute("data-key")));
+            k.addEventListener("click", (e) => playDrumKitAudio(e.currentTarget.getAttribute("data-key")));
+            k.addEventListener("transitionend", (e) => resetDrumkitKeyClass(e.target, e.propertyName));
         });
 
     }, false);
