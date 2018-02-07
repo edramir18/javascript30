d962e7b7502aae85d693e4dfbc35403c175ef14b
Eduardo Ramirez <edramir18@gmail.com>
2018-02-07 01:09:24 -0400

    Day 03 Initial Project

---
+++ day03/README.md
@@ -0,0 +1,9 @@
+# DAY 3 - CSS Variables
+How to use CSS variables and updated from javascript
+
+## Webbos Repository
+[Day 3](https://github.com/wesbos/JavaScript30/tree/master/03%20-%20CSS%20Variables)
+
+## Documentation
+- [CSS Variables](https://developer.mozilla.org/en-US/docs/Web/CSS/Using_CSS_variables)
+- [Element Dataset](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset)
---
+++ day03/gulpfile.js
@@ -0,0 +1,32 @@
+'use strict'
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const browserSync = require('browser-sync')
+
+// Task Definition
+// Sass Compilation
+gulp.task('sass', sassCompilation)
+// Server configuration for Browser-Sync
+gulp.task('serve', ['sass'], serveTask)
+// Default task to run with command gulp
+gulp.task('default', ['serve'])
+
+// Function Definition
+function sassCompilation () {
+  gulp.src('./src/scss/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourceComments: false
+    }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
+}
+
+function serveTask () {
+  browserSync.init({
+    server: './public'
+  })
+  gulp.watch('./src/scss/*.scss', ['sass'])
+  gulp.watch('./public/js/*.js').on('change', browserSync.reload())
+  gulp.watch('./public/*.html').on('change', browserSync.reload())
+}
